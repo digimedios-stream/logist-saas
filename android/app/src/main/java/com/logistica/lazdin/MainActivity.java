7b97c65501713d0496cb7e3707251cffe78aa369
Digimedios Apps <digimediosapps@gmail.com>
2026-09-11 00:06:33 -0300

    perf(android): configure hardware layer in MainActivity and disable PWA service worker on native APK

--- android/app/src/main/java/com/logistica/lazdin/MainActivity.java
+++ android/app/src/main/java/com/logistica/lazdin/MainActivity.java
@@ -1,5 +1,27 @@
 package com.logistica.lazdin;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            WebView webView = getBridge().getWebView();
+            WebSettings settings = webView.getSettings();
+            settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+            settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+            settings.setDomStorageEnabled(true);
+            settings.setDatabaseEnabled(true);
+            webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null);
+        }
+    }
+}
